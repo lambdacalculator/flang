e4ceae0dd82ae58a31dcb41ca2ee11c2230ce694
Todd Wilson <twilson@csufresno.edu>
2026-03-05 19:46:45 -0800

    Export all matchers, FSMs, and algorithms for student assignments

--- src/FLang.hs
+++ src/FLang.hs
@@ -1,20 +1,30 @@
 module FLang
-  ( -- * Regular Expressions
-    RegExp(..)
-  , (<+>), (<.>)
-  , toRE
-  , match3
+  ( -- * Regular Expressions & Matching
+    RegExp(..), Compact(..), toRE
+  , (<+>), (<.>), mkUnion, mkCat, mkStar
+  , match1, match2, match3, MProg(..), compile, runmp
+  , lq, (<\>), byp, nep, bypnep, numLets, starHgt, reverseRE
+  , lang_of, onestr, finite
+  
     -- * Finite State Machines
-  , FSM, NFSM, EFSM
-  , Machine(..), validate
-  , reachable, minimize, intify, nreachable
-
-  , toDot
-    -- * Algorithms
-  , brzozowski
+  , Machine(..), FSM, NFSM, EFSM
+  , accept, nacc
+  , reachable, nreachable, ereachable, validate, intify
+  , toDot, toDotN, buildTable, formatTable
+  , hat, star
+  
+    -- * Core Algorithms
+  , thompson, glushkov, brzozowski
+  , elim_eps, nfsm_to_fsm, reverseFSM, minimize, opFSM
+  , fsm_to_re, solve
+  
+    -- * Languages & Utilities
+  , Lang, lang, memb, strings
+  , uclosure, splits, power, cart, norm, diff
   ) where
-
 
 import FLang.RegExp
 import FLang.FSM
 import FLang.Algorithms
+import FLang.Language
+import FLang.Utils
